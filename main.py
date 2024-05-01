789ad0f6e46c828e031b46ebe376407ebb164232
Dicylow27 <emprendiendopocoapoco@gmail.com>
2024-05-02 00:01:34 +0200

    actualizacion

--- main.py
+++ main.py
@@ -1,13 +1,60 @@
-from my_chosen_backend import Application
 
+from fastapi import FastAPI
 from reactpy import component, html
-from reactpy.backend.my_chosen_backend import configure
+from reactpy.backend.fastapi import configure
 
 
 @component
-def HelloWorld():
-    return html.h1("Hello, world!")
+def Photo(alt_text, image_id):
+    return html.img(
+        {
+            "src": f"https://picsum.photos/id/{image_id}/500/300",
+            "style": {"width": "30%"},
+            "alt": alt_text,
+        }
+    )
 
 
-app = Application()
-configure(app, HelloWorld)
+@component
+def Gallery():
+    return html.section(
+        html.h1("Famous Musicians"),
+        Photo("Landscape", image_id=830),
+        Photo("City", image_id=274),
+        Photo("Puppy", image_id=237),
+    )
+
+
+@component
+def Item(name, done):
+        if done:
+            return html.li(name, " ✔")
+        else:
+            return html.li(name)
+
+
+@component
+def TodoList():
+    return html.section(
+        html.h1("My Todo List"),
+        html.ul(
+            Item("Encuentra un problema interesante para resolver", done=True),
+            Item("Crea una aplicación para resolverlo", done=False),
+            Item("¡Comparte esa aplicación con el mundo!", done=True),
+        ),
+    )
+
+
+
+
+@component
+def PagePrin():
+    return html.div(
+        html.h1("MIchael Araujo!"),
+        Gallery(),
+        TodoList()
+)
+    
+    
+app = FastAPI()
+configure(app, PagePrin)
